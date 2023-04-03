b654eecaa1091f150e43985225d7a9328ce11fc9
Tommy <39033375+TommyPhelps@users.noreply.github.com>
2023-04-03 17:32:47 -0500

    adding week 3 files

---
+++ week3/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿using System;
+
+namespace Module3Exercise2Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            // Changed these to the appropriate button names
+            this.btnRed = new System.Windows.Forms.Button();
+            this.btnBlue = new System.Windows.Forms.Button();
+            this.btnR = new System.Windows.Forms.Button();
+            this.btnL = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnRed
+            // 
+            this.btnRed.BackColor = System.Drawing.Color.Red;
+            this.btnRed.ForeColor = System.Drawing.Color.Red;
+            this.btnRed.Location = new System.Drawing.Point(21, 16);
+            this.btnRed.Name = "btnRed";
+            this.btnRed.Size = new System.Drawing.Size(93, 45);
+            this.btnRed.TabIndex = 0;
+            this.btnRed.Text = "&RED";
+            this.btnRed.UseVisualStyleBackColor = false;
+            this.btnRed.Click += new System.EventHandler(this.btnRed_Click);
+            // 
+            // btnBlue
+            // 
+            // Changed these to the appropriate button names
+            this.btnBlue.BackColor = System.Drawing.Color.Blue;
+            this.btnBlue.ForeColor = System.Drawing.Color.Blue;
+            this.btnBlue.Location = new System.Drawing.Point(376, 16);
+            this.btnBlue.Name = "btnBlue";
+            this.btnBlue.Size = new System.Drawing.Size(93, 45);
+            this.btnBlue.TabIndex = 1;
+            this.btnBlue.Text = "&BLUE";
+            this.btnBlue.UseVisualStyleBackColor = false;
+            this.btnBlue.Click += new System.EventHandler(this.btnBlue_Click);
+            // 
+            // btnR
+            // 
+            // Changed these to the appropriate button names
+            this.btnR.BackColor = System.Drawing.Color.Yellow;
+            this.btnR.Location = new System.Drawing.Point(376, 175);
+            this.btnR.Name = "btnR";
+            this.btnR.Size = new System.Drawing.Size(93, 45);
+            this.btnR.TabIndex = 3;
+            this.btnR.Text = "&R";
+            this.btnR.UseVisualStyleBackColor = false;
+            this.btnR.Click += new System.EventHandler(this.btnR_Click);
+            // 
+            // btnL
+            // 
+            // Changed these to the appropriate button names
+            this.btnL.BackColor = System.Drawing.Color.Yellow;
+            this.btnL.Location = new System.Drawing.Point(21, 175);
+            this.btnL.Name = "btnL";
+            this.btnL.Size = new System.Drawing.Size(93, 45);
+            this.btnL.TabIndex = 2;
+            this.btnL.Text = "&L";
+            this.btnL.UseVisualStyleBackColor = false;
+            this.btnL.Click += new System.EventHandler(this.btnL_Click);
+            // 
+            // btnExit
+            // 
+            // Changed these to the appropriate button names
+            this.btnExit.BackColor = System.Drawing.SystemColors.Control;
+            this.btnExit.Location = new System.Drawing.Point(130, 203);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(222, 61);
+            this.btnExit.TabIndex = 4;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Yellow;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(133, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(219, 184);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "label1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(481, 343);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnL);
+            this.Controls.Add(this.btnR);
+            this.Controls.Add(this.btnBlue);
+            this.Controls.Add(this.btnRed);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRed;
+        // Changed these to the appropriate button names
+        private System.Windows.Forms.Button btnBlue;
+        private System.Windows.Forms.Button btnR;
+        private System.Windows.Forms.Button btnL;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ week3/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Module3Exercise2Project
+{
+    public partial class Form1 : Form
+    {
+        private object lblMessage;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnR_Click(object sender, EventArgs e)
+        {
+            // Changed to label1.Text
+            label1.Text = btnR.Text;
+        }
+
+        private void btnRed_Click(object sender, EventArgs e)
+        {
+            // Changed to label1.Text
+            label1.BackColor = btnRed.BackColor;
+        }
+
+        private void btnBlue_Click(object sender, EventArgs e)
+        {
+            // Changed to label1.Text
+            label1.BackColor = btnBlue.BackColor;
+
+        }
+
+        private void btnL_Click(object sender, EventArgs e)
+        {
+            // Changed to label1.Text
+            label1.Text = btnL.Text;
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
